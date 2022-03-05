5d943fb5cefb607c212e374a63a6aa6d84b04bd4
Romain Neil <tutosfaciles48@gmail.com>
2022-03-05 16:19:31 +0100

    feat: define options

--- DomainUpdate/Program.cs
+++ DomainUpdate/Program.cs
@@ -4,6 +4,27 @@ namespace DomainUpdate
 {
 	class Program
 	{
+		class Options
+		{
+			[Option('d', "domain", Required = false)]
+			public string Domain { get; set; }
+			
+			[Option('f', "file", Required = false)]
+			public string FileName { get; set; }
+
+			[Option('u', "username", Required = false)]
+			public string Username { get; set; }
+			
+			[Option('p', "password", Required = false)]
+			public string Password { get; set; }
+			
+			[Option('c', "connector", Required = false, Default = "miab")]
+			public string Connector { get; set; }
+			
+			[Option('i', "instance", Required = false)]
+			public string Instance { get; set; }
+		}
+		
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
